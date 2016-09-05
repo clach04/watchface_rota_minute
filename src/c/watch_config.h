d0830aabbfa9a951f7523354eae99a49f6ed20aa
clach04 <Chris.Clark@actian.com>
2016-09-05 09:45:08 -0700

    Rect date position updated, slight shift left

--- src/c/watch_config.h
+++ src/c/watch_config.h
@@ -52,7 +52,7 @@
 //#define MAX_DATE_STR "Thu\n00\nAug" /* if custom version of DATE_FMT_STR is set, MAX_DATE_STR  needs to be updated too */
 #define DATE_FMT_STR "%b\n%d"
 #define MAX_DATE_STR "00\nAug" /* if custom version of DATE_FMT_STR is set, MAX_DATE_STR  needs to be updated too */
-#define DATE_POS GRect(0, -5, 144, 168) /* probably taller than really needed */
+#define DATE_POS GRect(0, -5, 140, 168) /* probably taller than really needed */
 
     #ifdef DRAW_BATTERY
         #define BAT_POS GRect(5, 150, 144, 168)
