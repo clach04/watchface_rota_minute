93bafeab3b849cf714e1cdf4d6739155d33cff3f
clach04 <clach04@gmail.com>
2026-02-17 16:09:47 -0800

    Move Phone battery demo location to avoid Emry conflict

--- src/c/watch_config.h
+++ src/c/watch_config.h
@@ -65,7 +65,7 @@
     #endif  // end of original rectangle size
 #endif /* end of Round or rectangle */
 
-#define PHONE_BAT_POS GRect(0, 40, 144, 168)  // TODO move/platform fix debug test - NOTE still depends on (watch) BAT_ALIGN - shrink this
+#define PHONE_BAT_POS GRect(0, 30, 144, 168)  // TODO move/platform fix debug test - NOTE still depends on (watch) BAT_ALIGN - shrink this
 
 /* for screen shots and font testing
 #define DEBUG  // If set will update each second and use seconds as minutes for checking updates (not the best for screenshots)
